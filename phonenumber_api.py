a02697c6615eb6807b82ad86e6fef0b11bfd7a23
LilBoGothBoy <74736733+LilBoGothBoy@users.noreply.github.com>
2022-03-04 17:51:55 +0300

    Create phonenumber_api.py

---
+++ phonenumber_api.py
@@ -0,0 +1,29 @@
+from flask_restful import Api, Resource
+from flask import Flask
+import json
+import logging
+import requests
+import sys
+
+
+app = Flask(__name__)
+api = Api(app)
+app.logger.addHandler(logging.StreamHandler(sys.stdout))
+app.logger.setLevel(logging.ERROR)
+
+
+class Bot_Answer(Resource):
+    def get(self, phone_number):
+        phone_number = phone_number.replace(" ", "")
+        response = requests.get(
+            f'http://rosreestr.subnets.ru/?get=num&num={phone_number}&format=json')
+        todos = json.loads(response.text)
+        if len(todos) == 11:
+            return todos['0']['region'], 200
+        else:
+            return 'Error!', 404
+
+
+api.add_resource(Bot_Answer, '/get_region/<string:phone_number>')
+if __name__ == '__main__':
+    app.run(debug=True)
